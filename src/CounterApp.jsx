19490871fe72d3b6ca1ec7f8ee3f1fe5c84fdd0e
Pedro <pedro.quinones@ias.com.co>
2023-03-22 11:41:41 -0500

    Implementación de useState en react

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import PropTypes from "prop-types";
 
-export const CounterApp = ({value}) => {
+export const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(value);
+
+  const handleAdd = () => {
+    // Ambas opciones son viables, en algunos casos será mejor utilizar el caso de función
+    // setCounter(counter + 1);
+    setCounter((counter) => counter + 5);
+  };
+
+  const handleSubtract = ()=>{
+    setCounter(counter - 1);
+  };
+
+  const handleReset = ()=>{
+    setCounter(value);
+  };
+
   return (
     <>
-        <h1>CounterApp</h1>
-        <h2>{value}</h2>
+      <h1>CounterApp</h1>
+      <h2>{counter}</h2>
+
+      <button onClick={handleAdd}>+ 1 </button>
+      <button onClick={handleSubtract}> - 1 </button>
+      <button onClick={handleReset}> Reset</button>
     </>
-  )
-}
+  );
+};
 
 CounterApp.propTypes = {
-    
-}
+  value: PropTypes.number.isRequired,
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import {HelloApp} from './HelloApp';
 import './style.css'
+import { CounterApp } from './CounterApp';
+// import {HelloApp} from './HelloApp';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <HelloApp/>
+        {/* <HelloApp/> */}
+        <CounterApp value={5}/>
     </React.StrictMode>
 )
 
